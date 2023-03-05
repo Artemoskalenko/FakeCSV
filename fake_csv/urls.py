73cc50718a0a4b0dce847d16f44f4b5ac9b5c7d1
Artemoskalenko <artem.moskalenko.00@gmail.com>
2023-03-05 02:43:43 +0200

    urls

--- fake_csv/urls.py
+++ fake_csv/urls.py
@@ -9,14 +9,14 @@ urlpatterns = [
 
     path('new_schema/', new_schema, name='new_schema'),
     path('<int:schema_id>/', edit_schema, name='edit_schema'),
-    path('/delete_schema/<int:schema_id>/', delete_schema, name='delete_schema'),
+    path('delete_schema/<int:schema_id>/', delete_schema, name='delete_schema'),
 
-    path('/<int:schema_id>/add_column/', add_column, name='add_column'),
-    path('/<int:schema_id>/edit_column/<int:column_id>/', edit_column, name='edit_column'),
-    path('/<int:schema_id>/delete_column/<int:column_id>/', delete_column, name='delete_column'),
+    path('<int:schema_id>/add_column/', add_column, name='add_column'),
+    path('<int:schema_id>/edit_column/<int:column_id>/', edit_column, name='edit_column'),
+    path('<int:schema_id>/delete_column/<int:column_id>/', delete_column, name='delete_column'),
 
-    path('/<int:schema_id>/data_sets/', data_sets_view, name='data_sets'),
-    path('/<int:schema_id>/data_sets/generate_data/', generate_data, name='generate_data'),
+    path('<int:schema_id>/data_sets/', data_sets_view, name='data_sets'),
+    path('<int:schema_id>/data_sets/generate_data/', generate_data, name='generate_data'),
 
     path('logout/', logout_user, name='logout'),
     path('login/', LoginUser.as_view(), name='login'),
